ce6b99bb8be19e0a7892804d21fb44ac98cbe20e
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2023-01-09 10:43:17 -0600

    Add files via upload
    Area of a rectangle challenge found on Nolan's Basic Ruby videos

---
+++ area.rb
@@ -0,0 +1,10 @@
+puts "Enter length"
+length = gets.chomp.to_i
+
+puts "Enter width"
+width = gets.chomp.to_i
+
+area = width * length
+
+puts "width is #{width}, length is #{length}"
+puts "area is #{area}"
