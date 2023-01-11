b4889fa8ec1b5ba7e991beb202cf14ab5c377e7d
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2023-01-10 20:59:44 -0600

    Ruby Basics - class one
    exercise 1.2

---
+++ basics_class_one/exercise_1.2.rb
@@ -0,0 +1,6 @@
+# Exercise 1.2: Use escaping to output single quotes in a string
+
+puts "'Single Quotes' is actually my favorite movie. It's great."
+
+#'Single Quotes' is actually my favorite movie. It's great.
+
