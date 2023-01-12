62b81b6633aa6f2ceec8ded1bbd4c132d8a2a998
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2023-01-12 13:44:34 -0600

    Ruby Basics - class two
    completed during independent work time in class on 01/11/2023

---
+++ basics_class_two/exercise_2.3.rb
@@ -0,0 +1,8 @@
+# > Exercise 2.3: Define a method with multiple arguments
+
+# Define a method called names_of_my_pets. Add three parameters to the method: name_1, name_2, name_3.
+def names_of_my_pets(name_1, name_2, name_3)
+    puts "I have 3 pets! Their names are #{name_1},#{name_2} and #{name_3}!"
+end
+
+names_of_my_pets('Lily', 'Ellie', "Karate Doom")
