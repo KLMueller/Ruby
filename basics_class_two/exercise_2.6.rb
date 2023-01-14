6ddf5eb38c686f737f11003b8fddbd9636fdf55e
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2023-01-14 14:35:21 -0600

    Basic Ruby - class two
    completed 01/14/2023

---
+++ basics_class_two/exercise_2.6.rb
@@ -0,0 +1,38 @@
+# Exercise 2.6: More built in array methods
+
+# Print out the array built in methods
+
+# puts [].methods
+
+# Create an array that includes three strings that resemble names: "Amy", "Jimmy", "Johnny"
+names =  ["Amy", "Jimmy", "Johnny"]
+##Use three different built in array methods that haven't been used today. Googling is recommended.
+puts ["Amy", "Jimmy", "Johnny"]
+ puts names.first
+ puts names.last
+ puts names.take(2)
+ puts names.pop
+ puts names.push ("Kelley")
+ puts names.shift
+ puts names.unshift
+ puts names.delete(1)
+ puts names.reverse
+
+# returns: 
+# Amy
+# Jimmy
+# Johnny
+# Amy
+# Johnny
+# Amy
+# Jimmy
+# Johnny
+# Amy
+# Jimmy
+# Kelley
+# Amy
+# Jimmy
+# Kelley
+
+# Kelley
+# Jimmy 
