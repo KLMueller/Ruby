7113d8330e209b714adce9643e4b5d15fdbca1a1
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2023-01-16 11:21:48 -0600

    Ruby_project
    beginning stages of creating a new Ruby project

---
+++ ruby_project/tv_tracker/lib/tv_tracker.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "tv_tracker/version"
+require_relative "cli.rb"
+
+module TvTracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
